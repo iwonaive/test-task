1c557c111bc3d1037176dfe5125c7b9d6ee93e59
iwonaive <szymaniak.iwona11@gmail.com>
2024-11-04 19:46:31 +0100

    final touches

---
+++ src/components/ResoultsNumber.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ResultsNumber.css";
+
+const ResultsNumber: React.FC = () => {
+  return <div className="results-number">Liczba wyników: 23</div>;
+};
+
+export default ResultsNumber;
+
+// here I should add later: type: number, const ResultsNumber { count } and show in return Liczba wyników: {count}
